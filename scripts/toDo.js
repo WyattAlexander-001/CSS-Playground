250bb267390bcc2a7df0b4b90cd638acc0ff1387
WyattAlexander-001 <wyattbushman117@gmail.com>
2023-08-16 03:53:13 -0400

    made a functioning to do list cli

--- scripts/toDo.js
+++ scripts/toDo.js
@@ -1,1 +1,18 @@
 console.log("Connected");
+let input = prompt("Welcome To The Todo List");
+const todos = ['Drink Water', "Study"];
+
+while (input != 'q' && input != 'quit') {
+    if (input === 'list') {
+        console.log('==========================');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(` #${i + 1} --- ${todos[i]}`);
+        }
+        console.log('==========================');
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, new todo?');
+        todos.push(newTodo);
+    }
+    input = prompt('What would you like to add?');
+}
+console.log('You quit');
